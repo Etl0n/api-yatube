f1efb5de8b415c803d599cf13cb746e53a11d9f0
Andrey <andreynokia04@gmail.com>
2023-05-20 14:15:18 +1000

    First_commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+
+app_name = 'api'
+
+router = SimpleRouter()
+router.register(r'v1/posts', PostViewSet)
+router.register(
+    r'v1/posts/(?P<post>[\d]+)/comments', CommentViewSet, basename='comments'
+)
+router.register(r'v1/follow', FollowViewSet, basename='follows')
+router.register(r'v1/groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
+    path('v1/', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,89 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import mixins
+from rest_framework import filters
+
+from .exceptions import PermissionDenied, FollowExit
+from .serializers import (
+    GroupSerializer,
+    FollowSerializer,
+    PostSerializer,
+    CommentSerializer,
+)
+from posts.models import Group, Follow, Post, Comment, User
+
+
+class ListCreateViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class RetrieveListViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.save(author=self.request.user)
+
+    def perform_destroy(self, serializer):
+        if self.request.user != serializer.author:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.delete()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(post=self.kwargs.get('post'))
+        return queryset
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(id=self.kwargs.get('post'))
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_destroy(self, serializer):
+        if self.request.user != serializer.author:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.delete()
+
+
+class FollowViewSet(ListCreateViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        follow = Follow.objects.filter(user=self.request.user)
+        return follow
+
+    def perform_create(self, serializer):
+        user = User.objects.get(username=self.request.data.get('following'))
+        follow = Follow.objects.filter(
+            following=user, user=self.request.user
+        ).exists()
+        if follow or (user == self.request.user):
+            raise FollowExit('Такой пост уже существует')
+        serializer.save(user=self.request.user)
+
+
+class GroupViewSet(RetrieveListViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
